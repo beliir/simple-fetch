b9507ea6e3536e022a0f9b7d01595691b62408eb
Igor Agapov <aio350@jetstyle.ru>
2021-07-29 11:40:07 +0500

    update

---
+++ npm/README.md
@@ -0,0 +1,91 @@
+# Simple Fetch
+
+### Utility for easy use of the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API">Fetch API</a>
+
+- Returns final (parsed to JSON, text or raw) result, including custom errors
+- Can be cancelled (for example, if the request takes too long)
+- Contains `baseUrl` setter
+
+## Installation
+
+```bash
+yarn add very-simple-fetch
+# or
+npm i very-simple-fetch
+```
+
+## Usage
+
+```js
+import simpleFetch from 'very-simple-fetch'
+
+const getTodos = async () => {
+  const todos = await simpleFetch('https://jsonplaceholder.typicode.com/todos')
+  console.table(todos)
+}
+getTodos()
+```
+
+[More examples on CodeSanbox](https://codesandbox.io/s/simple-fetch-1o2k)
+
+## You can import module directly from GitHub Pages
+
+```js
+import simpleFetch from 'https://harryheman.github.io/simple-fetch/simpleFetch.js'
+```
+
+## Signature of the main function
+
+```js
+simpleFetch(options: string | object)
+// alias for simpleFetch.get()
+```
+
+## Signatures of helper functions
+
+```js
+// GET
+simpleFetch.get(url: string, options: object)
+// POST
+simpleFetch.post(url: string, body: any, options: object)
+// PUT
+simpleFetch.update(url: string, body: any, options: object)
+// DELETE
+simpleFetch.remove(url: string, options: object)
+```
+
+## Cancellation of the request
+
+```js
+simpleFetch.cancel()
+```
+
+## Setting base URL
+
+```js
+simpleFetch.baseUrl = 'https://some-url.com'
+```
+
+## Options
+
+- <a href="https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch#parameters" target="_blank">common</a>
+- custom:
+  - params: object - this object is converted to encoded search parameters that are appended to the URL:
+    - key: string
+    - value: string
+  - customCache: boolean - if `true`, result of the GET-request will be stored in the local cache  - <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map">Map</a>. Result of the same request will be retrieved from this cache until `customCache` is set to `false`
+  - log: boolean - if `true`, options, cache and result will be output to the concole
+  - handlers: object:
+    - onSuccess: function
+    - onError: function
+    - onAbort: function
+
+## Response
+
+- data: any | null - result of the response or `null` if there war an error
+- error: null | any - `null` or custom error
+- info: object:
+  - headers: object
+  - status: number,
+  - statusText: string
+  - url: string
---
+++ npm/index.js
@@ -0,0 +1,199 @@
+const simpleFetchCache = new Map()
+
+let simpleFetchController = new window.AbortController()
+
+const simpleFetch = async (options) => {
+  let url = ''
+
+  if (typeof options === 'string') {
+    url = options
+  } else {
+    url = options?.url
+  }
+
+  if (simpleFetch.baseUrl) {
+    if (!url) {
+      url = simpleFetch.baseUrl
+    } else {
+      url =
+        url.indexOf('/') === 0
+          ? `${simpleFetch.baseUrl}${url}`
+          : `${simpleFetch.baseUrl}/${url}`
+    }
+  }
+
+  if (options?.params) {
+    url = Object.entries(options.params)
+      .reduce((a, [k, v]) => {
+        a += `&${k}=${v}`
+        return a
+      }, url)
+      .replace('&', '?')
+  }
+
+  url = window.encodeURI(url)
+
+  if (!url) {
+    return console.error('URL not provided!')
+  }
+
+  let defaultOptions = {
+    method: options?.method || 'GET',
+    headers: options?.headers || {
+      'Content-Type': 'application/json'
+    },
+    referrerPolicy: 'no-referrer',
+    customCache: options?.customCache ?? true,
+    log: options?.log || false,
+    signal: simpleFetchController.signal
+  }
+
+  if (options?.body) {
+    if (defaultOptions.headers['Content-Type'] === 'application/json') {
+      defaultOptions.body = JSON.stringify(options.body)
+    } else {
+      defaultOptions.body = options.body
+    }
+  }
+
+  if (typeof options === 'object') {
+    defaultOptions = {
+      ...options,
+      ...defaultOptions
+    }
+  }
+
+  if (options?.log) {
+    console.log(`%c Options: ${JSON.stringify(defaultOptions)}`, 'color: blue')
+  }
+
+  const handlers = options?.handlers
+
+  if (handlers?.onAbort) {
+    simpleFetchController.signal.addEventListener('abort', handlers.onAbort, {
+      once: true
+    })
+  }
+
+  const isCacheEnabled =
+    defaultOptions.method === 'GET' && defaultOptions.customCache
+
+  if (isCacheEnabled && simpleFetchCache.has(url)) {
+    const data = simpleFetchCache.get(url)
+    return handlers?.onSuccess ? handlers.onSuccess(data) : data
+  }
+
+  const isBodyNotProvided =
+    (defaultOptions.method === 'POST' || defaultOptions.method === 'PUT') &&
+    !defaultOptions.body
+
+  if (isBodyNotProvided) {
+    console.warn('Body not provided')
+  }
+
+  try {
+    const response = await fetch(url, defaultOptions)
+
+    const { status, statusText } = response
+
+    const info = {
+      headers: [...response.headers.entries()].reduce((a, [k, v]) => {
+        a[k] = v
+        return a
+      }, {}),
+      status,
+      statusText,
+      url: response.url
+    }
+
+    let data = null
+
+    if (response.headers.get('Content-Type')?.includes('json')) {
+      data = await response.json()
+    } else if (response.headers.get('Content-Type')?.includes('text')) {
+      data = await response.text()
+    } else {
+      data = response
+    }
+
+    let result = null
+
+    if (response.ok) {
+      result = { data, error: null, info }
+
+      if (defaultOptions.method === 'GET') {
+        simpleFetchCache.set(url, result)
+        if (options?.log) {
+          console.log(simpleFetchCache)
+        }
+      }
+
+      if (options?.log) {
+        console.log(`%c Result: ${JSON.stringify(result)}`, 'color: green')
+      }
+
+      return handlers?.onSuccess ? handlers.onSuccess(result) : result
+    }
+
+    result = {
+      data: null,
+      error: data,
+      info
+    }
+
+    if (options?.log) {
+      console.log(`%c Result: ${JSON.stringify(result)}`, 'color: red')
+    }
+
+    return result
+  } catch (err) {
+    if (handlers?.onError) {
+      return handlers.onError(err)
+    }
+    console.error(err)
+  }
+}
+
+let baseUrl = ''
+Object.defineProperty(simpleFetch, 'baseUrl', {
+  get: () => baseUrl,
+  set: (url) => {
+    baseUrl = url
+  }
+})
+
+simpleFetch.cancel = () => {
+  simpleFetchController.abort()
+  simpleFetchController = new window.AbortController()
+}
+
+simpleFetch.get = (url, options) =>
+  simpleFetch({
+    url,
+    ...options
+  })
+
+simpleFetch.post = (url, body, options) =>
+  simpleFetch({
+    url,
+    method: 'POST',
+    body,
+    ...options
+  })
+
+simpleFetch.update = (url, body, options) =>
+  simpleFetch({
+    url,
+    method: 'PUT',
+    body,
+    ...options
+  })
+
+simpleFetch.remove = (url, options) =>
+  simpleFetch({
+    url,
+    method: 'DELETE',
+    ...options
+  })
+
+export default simpleFetch
